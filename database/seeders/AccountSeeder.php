b38d8e356230a0db9e23cc17416a43d81cfd8fbf
renzoDC15 <renzo.carianga@gmail.com>
2024-11-08 12:22:52 +0800

    add Account Seeder

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class AccountSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       Account::updateOrCreate(['name' => 'Pagsikat Trips','code'=>'pgskt'],['description' => 'Pagsikat']);
+       Account::updateOrCreate(['name' => 'Pagsibol Trips','code'=>'pgsbt'],['description' => 'Pagsibol']);
+       Account::updateOrCreate(['name' => 'Pasinaya Trips','code'=>'psnyt'],['description' => 'Pasinaya']);
+
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-           UserSeeder::class,
-           ProjectSeeder::class,
-           CompanySeeder::class,
-           DepartmentSeeder::class,
-           CarTypeSeeder::class
+            UserSeeder::class,
+            ProjectSeeder::class,
+            CompanySeeder::class,
+            DepartmentSeeder::class,
+            CarTypeSeeder::class,
+            AccountSeeder::class
         ]);
     }
 }
