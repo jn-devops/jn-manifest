92e8893d0ea1901b05abad52342d6dd91bd4c268
renzoDC15 <renzo.carianga@gmail.com>
2024-11-08 12:05:49 +0800

    Company Seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class CompanySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Company::updateOrCreate(['name' => 'Joy Nostalg']);
+        Company::updateOrCreate(['name' => 'Raemuland Lands Inc.']);
     }
 }
