49c696424da6d9da4cac22c278fdcf575aed40fd
renzoDC15 <renzo.carianga@gmail.com>
2024-11-08 12:02:47 +0800

    CarType Seeder

--- database/seeders/CarTypeSeeder.php
+++ database/seeders/CarTypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CarType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,8 @@ class CarTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CarType::updateOrCreate(['name' => 'Sedan'], ['description' => 'Sedan']);
+        CarType::updateOrCreate(['name' => 'SUV'], ['description' => 'SUV','capacity' => 7]);
+        CarType::updateOrCreate(['name' => 'Van'], ['description' => 'Van','capacity' => 9]);
     }
 }
