b0989dd69e9606bfee6a212fd7cdf58fa23837c9
jn-devops <devops@joy-nostalg.com>
2024-12-19 15:26:07 +0800

    add wallet float and payment

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\TripTicket;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Project;
@@ -22,4 +24,19 @@ test('project has fillables', function () {
     expect($project->code)->toBe($code);
     expect($project->name)->toBe($name);
     expect($project->description)->toBe($description);
+});
+
+
+test('project has wallet', function () {
+    $project = Project::factory()->create();
+    $transaction = $project->depositFloat(100, null, false);
+    expect($transaction->confirmed)->toBeFalse();
+    $project->confirm($transaction);
+    expect($transaction->confirmed)->toBeTrue();
+//    $project->forceWithdrawFloat(250);
+    expect((float)$project->balanceFloat)->toBe(100.0);
+    $trip_ticket = TripTicket::factory()->forUser()->create();
+
+    $project->pay($trip_ticket);
+    expect((float)$project->balanceFloat)->toBe(99.0);
 });
