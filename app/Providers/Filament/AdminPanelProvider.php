6ddda3b67a2fcae5f4c1cbd2226c3631782f315d
renzoDC15 <renzo.carianga@gmail.com>
2025-01-15 11:58:11 +0800

    maxContentWidth

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -30,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->databaseNotifications()
             ->passwordReset()
             ->databaseNotificationsPolling(60)
+            ->maxContentWidth(MaxWidth::Full)
             ->colors([
                 'primary' => Color::Amber,
             ])
